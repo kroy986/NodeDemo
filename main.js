31a92ae8458250747ecbfe15327bfc378614a141
Koushik <kroy700133@gmail.com>
2018-08-10 01:58:24 +0530

    mainchgs

---
+++ main.js
@@ -0,0 +1,17 @@
+let express=require('express');
+let port=8000;
+let routes = require('./controller/userController');
+let router  = express.Router();
+let app=express();
+let bodyParser=require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+routes.app(app);
+
+app.listen(process.env.PORT || port,function(req,res,next){
+  console.log("Server is Running on ");
+});
