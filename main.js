f77aaa567f5fde963c71db07a941e640d159f64a
kroy986 <38892285+kroy986@users.noreply.github.com>
2020-08-08 22:34:54 +0530

    Update main.js

--- main.js
+++ main.js
@@ -14,5 +14,5 @@ app.use(bodyParser.json())
 routes.app(app);
 
 app.listen(process.env.PORT || port,function(req,res,next){
-  console.log("Server is Running on ");
+  console.log("Server is Running on");
 });
